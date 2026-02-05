b1eb62832a1ff4b33d401954b064de59af9887d4
Jimuel818 <jimuel684@gmail.com>
2026-02-05 21:39:05 +0800

    First code change for later

---
+++ LaboratoryTest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class LaboratoryTest{
+    public static void main(String[]args){
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("=========Laboratory Diagnostic==========");
+        System.out.println("1.Add Patient");
+        System.out.println("2.Chech Patients ");//1.Male,2.female,3.all
+        System.out.println("3.CLINICAL CHEMISTRY TEST");
+        System.out.println("4.Exit");
+        System.out.print("Enter your choice: ");
+        int choice = nextInt();
+
+
+        
+
+    
+
+
+    
+        
+
+
+
+    }
+}
